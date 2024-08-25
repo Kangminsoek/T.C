881ae37d4a4ddc0ab37b6673b9fe1971d61531d1
wodud892 <kojaeyoung81@gmail.com>
2024-08-26 05:23:11 +0900

    Update recommended_course_screen.dart

--- screen/recommended_course_screen.dart
+++ screen/recommended_course_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+//ㅇㄴㅁㅇ
 class RecommendedCourseScreen extends StatelessWidget {
   final LatLng _initialPosition = LatLng(37.206821,127.033268);
 
