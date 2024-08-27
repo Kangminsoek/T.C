af492d40dcf48a838ffb1ba24eafd9dfccd9a06a
wodud892 <kojaeyoung81@gmail.com>
2024-08-28 08:45:10 +0900

    Update main.dart

--- screen/main.dart
+++ screen/main.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Firebase Core import
+import 'package:http/http.dart' as http; // HTTP 패키지 import
+import 'dart:convert'; // JSON 디코딩을 위한 패키지 import
+
 import 'package:cppick/screen/splash_screen.dart';
 import 'package:cppick/screen/login_screen.dart';
 import 'package:cppick/screen/main_screen.dart';
@@ -34,7 +37,55 @@ class MyApp extends StatelessWidget {
         '/category': (context) => CategoryScreen(),
         '/mbti': (context) => MBTIScreen(),
         '/event': (context) => EventScreen(),
+        '/courses': (context) => CourseListScreen(), // 새로운 코스 화면 추가
       },
+    );
+  }
+}
+
+class CourseListScreen extends StatefulWidget {
+  @override
+  _CourseListScreenState createState() => _CourseListScreenState();
+}
+
+class _CourseListScreenState extends State<CourseListScreen> {
+  List<dynamic> _courses = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCourses();
+  }
+
+  Future<void> fetchCourses() async {
+    final response = await http.get(Uri.parse('http://your-server-ip:5000/api/courses'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _courses = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load courses');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Courses'),
+      ),
+      body: _courses.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _courses.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_courses[index]['name']),
+                  subtitle: Text(_courses[index]['description']),
+                );
+              },
+            ),
     );
   }
 }
